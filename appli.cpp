0b98e4cee812e0da3e891e244000bdb7086bd4c5
humblot quentin <quentinhbt66@gmail.com>
2023-01-12 08:59:56 +0100

    version double

--- appli.cpp
+++ appli.cpp
@@ -2,8 +2,9 @@
 
 int main (void)
 {
-    std::cout << "Bonjour !" << std::endl;
-	std::cout << "humblot quentin" << std::endl;
-    
+	for(int i = 0 ; i = 2 ; i++){
+		std::cout << "Bonjour !" << std::endl;
+		std::cout << "humblot quentin" << std::endl;
+	}
     return 0;
 }
